2edbba04f4c0f91483226cd2f54f9a58d4a770a2
Vetle Limbodal <vwlimbo@proton.me>
2024-10-26 17:34:14 +0200

    Add queryselector to all input elemts with all
    Use query selector all to handle all buttons under same node list.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // Variables
+const allInputs = document.querySelectorAll('.main__input');
 const topLeft = document.querySelector('.main__input--top-left');
 const topRight = document.querySelector('.main__input--top-right');
 const bottomLeft = document.querySelector('.main__input--bottom-left');
 const bottomRight = document.querySelector('.main__input--bottom-right');
 
-console.log(topLeft);
-console.log(topRight);
-console.log(bottomLeft);
-console.log(bottomRight);
+// Functions
+const addEventListeners = () => {
+    for (let input of inputs
+}
+
+
+// Event-listeners
