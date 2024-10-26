b94bbd0959c7755a630863357548b4d7cce67bc8
Vetle Limbodal <vwlimbo@proton.me>
2024-10-26 17:24:44 +0200

    Add query selectors for all input boxes
    All input boxes now has their own query selector in JS.

---
+++ app.js
@@ -0,0 +1,10 @@
+// Variables
+const topLeft = document.querySelector('.main__input--top-left');
+const topRight = document.querySelector('.main__input--top-right');
+const bottomLeft = document.querySelector('.main__input--bottom-left');
+const bottomRight = document.querySelector('.main__input--bottom-right');
+
+console.log(topLeft);
+console.log(topRight);
+console.log(bottomLeft);
+console.log(bottomRight);
