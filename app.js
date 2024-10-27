043d989e5a5d503afad91f05f971583f0d3261b6
Vetle Limbodal <vwlimbo@proton.me>
2024-10-27 19:18:29 +0100

    Create function that applies event listeners
    Now a function will automatically add event listeners on all input bars.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // Variables
 const allInputs = document.querySelectorAll('.main__input');
-const topLeft = document.querySelector('.main__input--top-left');
-const topRight = document.querySelector('.main__input--top-right');
-const bottomLeft = document.querySelector('.main__input--bottom-left');
-const bottomRight = document.querySelector('.main__input--bottom-right');
 
 // Functions
 const addEventListeners = () => {
-    for (let input of inputs
-}
-
-
+    for (let input of allInputs) {
+        input.addEventListener('keydown', (event) => {
+            console.log(event.key);
+        });
+    };
+};
 // Event-listeners
+addEventListeners();
